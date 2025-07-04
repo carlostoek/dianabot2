a8341bd4644112942f0844f6b0c1af82952b569a
carlostoek <carlostoek@gmail.com>
2025-07-03 19:59:01 -0600

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_PATH = os.getenv("DATABASE_PATH", "bot_database.db")
 
 if not BOT_TOKEN:
     raise Exception("BOT_TOKEN no encontrado. Verifica tus variables de entorno.")
